f2405afce316e2dd8750237ce2a60290e6de5435
Leonardo Rodrigues Lima de Souza Pinto <Lrodlima@users.noreply.github.com>
2017-04-20 12:09:58 -0300

    Update 04_01_download_length.py
    Fix a bug for Python 3.6 using Anaconda

--- 04_01_download_length.py
+++ 04_01_download_length.py
@@ -1,6 +1,6 @@
 BUFF_SIZE = 1024
 def download_length(response, output, length):
-    times = length / BUFF_SIZE
+    times = int(length / BUFF_SIZE)
     if length % BUFF_SIZE > 0:
         times += 1
     for time in range(times):
